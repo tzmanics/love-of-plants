9524b1fd33db4c0f24f31b232d8dad1a5c9f89f1
tzmanics <tzmanics@gmail.com>
2018-07-16 13:53:28 -0400

    separate style from plant.js & add more display fields

--- client/src/Plant.js
+++ client/src/Plant.js
@@ -2,28 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types'
 import { compose, flattenProp, withHandlers, pure } from 'recompose'
 import { withFirestore } from './utils';
-
-const styles = {
-  container: {
-    alignItems: 'center',
-    border: '1px solid lightgrey',
-    float: 'left',
-    display: 'flex',
-    margin: '1rem',
-    padding: '.25rem',
-    width: '200px'
-  },
-  image: {
-    width: '200px'
-  },
-  owner: {
-    fontSize: '.6rem'
-  },
-  meta: {
-    display: 'flex',
-    flexDirection: 'column'
-  }
-}
+import './Plant.css'
 
 const Plant = ({
   family,
@@ -39,12 +18,18 @@ const Plant = ({
   waterFrequency,
   onWaterClick
 }) => (
-  <div style={styles.container}>
-    <div style={styles.meta}>
-      <img src={imageUrl} alt={name} style={styles.image}/>
-      <h3>{name}</h3>
-      <h5 style={styles.owner}>{location}</h5>
-    </div>
+  <div className="container">
+    <ul className="plant-info">
+      <li><img src={imageUrl} alt={name}/></li>
+      <li><h3>{name}</h3></li>
+      <li><h5>{family}</h5></li>
+      <li><h5>{fertilizerFrequency}</h5></li>
+      <li><h5>{waterFrequency}</h5></li>
+      <li><h5>{lightAmount}</h5></li>
+      <li><h5>{lightNeeds}</h5></li>
+      <li><h5>{location}</h5></li>
+      <li className="tips"><h5>{tips}</h5></li>
+    </ul>
   </div>
 );
 
