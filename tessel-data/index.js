fe2722cd97520640b9d4cddd7a4534a15c6f7b7b
tzmanics <tzmanics@gmail.com>
2018-07-17 14:43:14 -0400

    add firebase info/WIP

--- tessel-data/index.js
+++ tessel-data/index.js
@@ -1,18 +1,39 @@
 'use strict';
 
-// Import the interface to Tessel hardware
 const tessel = require('tessel');
 const ambientlib = require('ambient-attx4');
+const firebase = require('firebase');
 
 const ambient = ambientlib.use(tessel.port['A']);
 const moisture = tessel.port.B.pin[7];
+
+const config = {
+  apiKey: "AIzaSyCTYB1-h-j2wBbksyKXKWQNmac-T3tpwYA",
+  authDomain: "love-of-plants.firebaseapp.com",
+  databaseURL: "https://love-of-plants.firebaseio.com",
+  projectId: "love-of-plants",
+  storageBucket: "love-of-plants.appspot.com",
+  messagingSenderId: "255399180971"
+}
+
+const db = firebase.database();
+const ref = db.ref('plants');
+const lightSensorRef = ref.child('sensor/lightSensor');
+
+firebase.initializeApp(config);
 
 ambient.on('ready', function () {
   ambient.setLightTrigger(0.1);
 })
 
 ambient.on('light-trigger', function (data) {
+   lightSensorRef.push().set({
+    data: data.toFixed(4) * 100,
+    time: Date.now()
+  });
+
   console.log('The plant is getting direct sun ☀:', data)
+
   ambient.clearLightTrigger();
   setTimeout(function () {
     ambient.setLightTrigger(0.1);
